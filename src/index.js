b3ab96e49678684bf9bf7db22aa1cc6710e4c00c
FinloAnne <annexxx25@gmail.com>
2025-01-13 21:57:59 +0200

    Updated Javascript and form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello world");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "The Raven by Edgar Allan Poe",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+let poemFormComponent = document.querySelector("#poem-generator-form");
+poemFormComponent.addEventListener("submit", generatepoem);
